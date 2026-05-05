273b45df299a1ca5374ea32994782934ab92ff17
luan-filipin <luan.filipin@hotmail.com>
2026-05-05 10:29:50 -0300

    - Commit inicial. - Configurado a conexão com o banco postgres. - Configura variable environment para que nao seja necessário incluir os dados de acesso ao banco direto no projeto.

---
+++ gestao-de-hotel/src/main/java/com/api/gestaodehotel/GestaoDeHotelApplication.java
@@ -0,0 +1,13 @@
+package com.api.gestaodehotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoDeHotelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestaoDeHotelApplication.class, args);
+    }
+
+}
---
+++ gestao-de-hotel/src/test/java/com/api/gestaodehotel/GestaoDeHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.gestaodehotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoDeHotelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
